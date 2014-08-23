bcc3484302850e0ee0d69aa9cb671feb102bb127
Kevin Greene <kgreenek@gmail.com>
2014-08-23 14:03:37 -0700

    Adding missing dep from OSX tests.

--- BitcoinSwiftOSX/BitcoinSwiftOSX.h
+++
@@ -1,9 +0,0 @@
-//
-//  BitcoinSwiftOSX.h
-//  BitcoinSwiftOSX
-//
-//  Created by Kevin Greene on 8/23/14.
-//  Copyright (c) 2014 DoubleSha. All rights reserved.
-//
-
-#import <BitcoinSwift/BitcoinSwift.h>
