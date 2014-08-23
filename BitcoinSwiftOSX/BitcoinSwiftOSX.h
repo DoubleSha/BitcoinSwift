9eb8bbda7c6e593c179aaf33de192be19482045e
Kevin Greene <kgreenek@gmail.com>
2014-08-23 13:59:11 -0700

    Adding target for OSX and OSX tests.

---
+++ BitcoinSwiftOSX/BitcoinSwiftOSX.h
@@ -0,0 +1,9 @@
+//
+//  BitcoinSwiftOSX.h
+//  BitcoinSwiftOSX
+//
+//  Created by Kevin Greene on 8/23/14.
+//  Copyright (c) 2014 DoubleSha. All rights reserved.
+//
+
+#import <BitcoinSwift/BitcoinSwift.h>
