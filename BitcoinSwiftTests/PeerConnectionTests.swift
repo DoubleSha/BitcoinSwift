21bed6e1bb1686dd5218e402b89fa6e0ece264ce
Kevin Greene <kgreenek@gmail.com>
2014-08-31 22:23:53 -0700

    Fixing test targets so syntax highlighting in xcode works.

---
+++ BitcoinSwiftTests/PeerConnectionTests.swift
@@ -0,0 +1,14 @@
+//
+//  PeerConnectionTests.swift
+//  BitcoinSwift
+//
+//  Created by Kevin Greene on 8/31/14.
+//  Copyright (c) 2014 DoubleSha. All rights reserved.
+//
+
+import BitcoinSwift
+import XCTest
+
+class PeerConnectionTests: XCTestCase {
+
+}
