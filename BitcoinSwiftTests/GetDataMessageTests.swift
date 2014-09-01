81a0c012021e4be81e3a4c5571284a6aa2dedaf1
James MacWhyte <keatonatron@gmail.com>
2014-08-31 22:27:13 -0700

    Added GetData message with tests, updated Inventory Vectors and test data.

--- BitcoinSwiftTests/GetDataMessageTests.swift
+++ BitcoinSwiftTests/GetDataMessageTests.swift
@@ -1,19 +1,19 @@
 //
-//  InventoryMessageTests.swift
+//  GetDataMessageTests.swift
 //  BitcoinSwift
 //
-//  Created by James MacWhyte on 14/23/8.
+//  Created by James MacWhyte on 14/31/8.
 //  Copyright (c) 2014 DoubleSha. All rights reserved.
 //
 
 import BitcoinSwift
 import XCTest
 
-class InventoryMessageTests: XCTestCase {
+class GetDataMessageTests: XCTestCase {
 
   // TODO: Add edge test cases: Too many vectors, empty data, etc.
 
-  func testInventoryMessageDecoding() {
+  func testGetDataMessageDecoding() {
     let bytes: [UInt8] = [
         0x01,                                           // Number of inventory vectors (1)
         0x02, 0x00, 0x00, 0x00,                         // First vector type (2: Block)
@@ -22,23 +22,23 @@ class InventoryMessageTests: XCTestCase {
         0xa3, 0xa2, 0x51, 0xc2, 0x13, 0x40, 0x21, 0x1e,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
     let data = NSData(bytes:bytes, length:bytes.count)
-    if let inventoryMessage = InventoryMessage.fromData(data) {
-      XCTAssertEqual(inventoryMessage.inventoryVectors.count, 1)
+    if let getDataMessage = GetDataMessage.fromData(data) {
+      XCTAssertEqual(getDataMessage.inventoryVectors.count, 1)
       let vector1Hash: [UInt8] = [
           0x71, 0x40, 0x03, 0x91, 0x50, 0x8c, 0xae, 0x45, // Block hash
           0x35, 0x86, 0x4f, 0x74, 0x91, 0x76, 0xab, 0x7f,
           0xa3, 0xa2, 0x51, 0xc2, 0x13, 0x40, 0x21, 0x1e,
           0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-      let expectedInventoryVectors = [
+      let expectedGetDataVectors = [
           InventoryVector(type:InventoryVector.VectorType.Block,
                           hash:NSData(bytes:vector1Hash, length:vector1Hash.count))]
-      XCTAssertEqual(inventoryMessage.inventoryVectors, expectedInventoryVectors)
+      XCTAssertEqual(getDataMessage.inventoryVectors, expectedGetDataVectors)
     } else {
-      XCTFail("\n[FAIL] Failed to parse InventoryMessage")
+      XCTFail("\n[FAIL] Failed to parse GetDataMessage")
     }
   }
 
-  func testInventoryMessageEncoding() {
+  func testGetDataMessageEncoding() {
     let vector1Hash: [UInt8] = [
         0x71, 0x40, 0x03, 0x91, 0x50, 0x8c, 0xae, 0x45, // Block hash
         0x35, 0x86, 0x4f, 0x74, 0x91, 0x76, 0xab, 0x7f,
@@ -47,7 +47,7 @@ class InventoryMessageTests: XCTestCase {
     let inventoryVectors = [
         InventoryVector(type:InventoryVector.VectorType.Block,
                         hash:NSData(bytes:vector1Hash, length:vector1Hash.count))]
-    let inventoryMessage = InventoryMessage(inventoryVectors:inventoryVectors)
+    let getDataMessage = GetDataMessage(inventoryVectors:inventoryVectors)
     let expectedBytes: [UInt8] = [
         0x01,                                           // Number of inventory vectors (1)
         0x02, 0x00, 0x00, 0x00,                         // First vector type (2: Block)
@@ -56,6 +56,6 @@ class InventoryMessageTests: XCTestCase {
         0xa3, 0xa2, 0x51, 0xc2, 0x13, 0x40, 0x21, 0x1e,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
     let expectedData = NSData(bytes:expectedBytes, length:expectedBytes.count)
-    XCTAssertEqual(inventoryMessage.data, expectedData)
+    XCTAssertEqual(getDataMessage.data, expectedData)
   }
 }
