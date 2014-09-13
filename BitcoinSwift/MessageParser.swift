82b17ec7d40db86fcb9028371c78e58a245b3f8c
Kevin Greene <kgreenek@gmail.com>
2014-09-13 13:03:56 -0700

    Splitting the message parsing logic into a new class, MessageParser so it can be tested separately.

---
+++ BitcoinSwift/MessageParser.swift
@@ -0,0 +1,114 @@
+//
+//  MessageParser.swift
+//  BitcoinSwift
+//
+//  Created by Kevin Greene on 9/13/14.
+//  Copyright (c) 2014 DoubleSha. All rights reserved.
+//
+
+import Foundation
+
+public protocol MessageParserDelegate : class {
+  func didParseMessageWithHeader(header: Message.Header, payloadData: NSData)
+}
+
+/// Parses Messages from raw data received from the wire. Handles framing and ensuring message
+/// data integrety.
+public class MessageParser {
+
+  public weak var delegate: MessageParserDelegate? = nil
+
+  private let network: Message.Network
+
+  // We receive a message in chunks. When we have received only part of a message, but not the
+  // whole thing, receivedBytes stores the pending bytes, and adds onto it the next time
+  // we receive a NSStreamEvent.HasBytesAvailable event.
+  private var receivedBytes: [UInt8] = []
+
+  // The header for the message we are about to receive. This is non-nil when we have received
+  // enough bytes to parse the header, but haven't received the full message yet.
+  private var receivedHeader: Message.Header? = nil
+
+  public init(network: Message.Network) {
+    self.network = network
+  }
+
+  // Consumes the data in bytes by parsing it into Messages, or discarding invalid data.
+  // Doesn't return until all data in receivedBytes has been consumed, or until more data must be
+  // received to parse a valid message.
+  public func parseBytes(bytes: [UInt8]) {
+    receivedBytes += bytes
+    while true {
+      if receivedHeader == nil {
+        let headerStartIndex = headerStartIndexInBytes(receivedBytes)
+        if headerStartIndex < 0 {
+          // We did not find the message start, so we must wait for more bytes.
+          // Throw away the bytes we have since we couldn't figure out how to handle them.
+          // We might have all but the last byte of the networkMagicBytes for the next message,
+          // so keep the last 3 bytes.
+          let end = receivedBytes.count - network.magicBytes.count + 1
+          if end > 0 {
+            receivedBytes.removeRange(0..<end)
+          }
+          return
+        }
+        // Remove the bytes before startIndex since we don't know what they are.
+        receivedBytes.removeRange(0..<headerStartIndex)
+        if receivedBytes.count < Message.Header.length {
+          // We're expecting a header, but there aren't enough bytes yet to parse one.
+          // Wait for more bytes to be received.
+          return
+        }
+        let data = NSData(bytes:receivedBytes, length:receivedBytes.count)
+        receivedHeader = Message.Header.fromData(data)
+        if receivedHeader == nil {
+          // Failed to parse the header for some reason. It's possible that the networkMagicBytes
+          // coincidentally appeared in the byte data, or the header was invalid for some reason.
+          // Strip the networkMagicBytes so we can advance and try to parse again.
+          receivedBytes.removeRange(0..<network.magicBytes.count)
+          continue
+        }
+        // receivedHeader is guaranteed to be non-nil at this point.
+        // We successfully parsed the header from receivedBytes, so remove those bytes.
+        receivedBytes.removeRange(0..<Message.Header.length)
+      }
+      assert(Message.Header.length == 24)
+      // NOTE: payloadLength can be 0 for some message types, e.g. VersionAck.
+      let payloadLength = Int(receivedHeader!.payloadLength)
+      // TODO: Need to figure out a maximum length to allow here, or somebody could DOS us by
+      // providing a huge value for payloadLength.
+      if receivedBytes.count < payloadLength {
+        // Haven't received the whole message yet. Wait for more bytes.
+        return
+      }
+      let payloadData = NSData(bytes:receivedBytes, length:payloadLength)
+      delegate?.didParseMessageWithHeader(receivedHeader!, payloadData:payloadData)
+      receivedBytes.removeRange(0..<payloadLength)
+      receivedHeader = nil
+    }
+  }
+
+  // MARK: - Private Methods
+
+  // Returns -1 if the header start (the position of network.magicBytes) was not found.
+  // Otherwise returns the position where the message header begins.
+  private func headerStartIndexInBytes(bytes: [UInt8]) -> Int {
+    let networkMagicBytes = network.magicBytes
+    if bytes.count < networkMagicBytes.count {
+      return -1
+    }
+    for i in 0...(bytes.count - networkMagicBytes.count) {
+      var found = true
+      for j in 0..<networkMagicBytes.count {
+        if bytes[i + j] != networkMagicBytes[j] {
+          found = false
+          break
+        }
+      }
+      if found {
+        return i
+      }
+    }
+    return -1
+  }
+}
--- BitcoinSwift/PeerConnection.swift
+++ BitcoinSwift/PeerConnection.swift
@@ -32,7 +32,7 @@ public protocol PeerConnectionDelegate : class {
 /// All message sending, receiving, and serialization is done on a dedicated background thread.
 /// By default, delegate methods are dispatched on the main queue. A different queue may be used by
 /// passing a delegateQueue to the constructor.
-public class PeerConnection: NSObject, NSStreamDelegate {
+public class PeerConnection: NSObject, NSStreamDelegate, MessageParserDelegate {
 
   public enum Status { case NotConnected, Connecting, Connected, Disconnecting }
 
@@ -49,6 +49,8 @@ public class PeerConnection: NSObject, NSStreamDelegate {
   private let peerPort: UInt16
   private let network: Message.Network
 
+  private let messageParser: MessageParser
+
   // Streams used to read & write data from the connected peer.
   private var inputStream: NSInputStream!
   private var outputStream: NSOutputStream!
@@ -59,13 +61,6 @@ public class PeerConnection: NSObject, NSStreamDelegate {
   // happens, we must stash the remaining bytes and try again when we receive a
   // NSStreamEvent.HasBytesAvailable event from the outputStream.
   private var pendingSendBytes: [UInt8] = []
-  // We receive a message in chunks. When we have received only part of a message, but not the
-  // whole thing, receivedBytes stores the pending bytes, and adds onto it the next time
-  // we receive a NSStreamEvent.HasBytesAvailable event.
-  private var receivedBytes: [UInt8] = []
-  // The header for the message we are about to receive. This is non-nil when we have received
-  // enough bytes to parse the header, but haven't received the full message yet.
-  private var receivedHeader: Message.Header? = nil
   private var readBuffer = [UInt8](count:1024, repeatedValue:0)
 
   private let networkThread = Thread()
@@ -88,6 +83,9 @@ public class PeerConnection: NSObject, NSStreamDelegate {
     self.peerPort = port
     self.network = network
     self.delegateQueue = delegateQueue
+    self.messageParser = MessageParser(network:network)
+    super.init()
+    self.messageParser.delegate = self
   }
 
   public init(IP: IPAddress,
@@ -101,6 +99,9 @@ public class PeerConnection: NSObject, NSStreamDelegate {
     self.peerPort = port
     self.network = network
     self.delegateQueue = delegateQueue
+    self.messageParser = MessageParser(network:network)
+    super.init()
+    self.messageParser.delegate = self
   }
 
   /// Attempts to open a connection to the remote peer.
@@ -200,6 +201,51 @@ public class PeerConnection: NSObject, NSStreamDelegate {
     }
   }
 
+  // MARK: - MessageParserDelegate
+
+  // Parses the payload from payloadData given the provided header, and notifies the delegate if
+  // parsing was successful. For some message types (e.g. VersionAck), payloadData is expected to
+  // have a length of 0.
+  public func didParseMessageWithHeader(header: Message.Header, payloadData: NSData) {
+    // TODO: Add the rest of the messages.
+    println("Received \(header.command.toRaw()) message")
+    switch header.command {
+      case .Version:
+        if peerVersion != nil {
+          println("WARN: Received extraneous VersionMessage. Ignoring")
+          break
+        }
+        assert(status == .Connecting)
+        let versionMessage = VersionMessage.fromData(payloadData)
+        if versionMessage == nil {
+          disconnectWithError(errorWithCode(.Unknown))
+          break
+        }
+        if !isPeerVersionSupported(versionMessage!) {
+          disconnectWithError(errorWithCode(.UnsupportedPeerVersion))
+          break
+        }
+        peerVersion = versionMessage!
+        sendVersionAck()
+        if receivedVersionAck {
+          didConnect()
+        }
+      case .VersionAck:
+        if status != .Connecting {
+          // The connection might have been cancelled, or it might have failed. For example,
+          // the connection can fail if we received an invalid VersionMessage from the peer.
+          println("WARN: Ignoring VersionAck message because not in Connecting state")
+          return
+        }
+        receivedVersionAck = true
+        if peerVersion != nil {
+          didConnect()
+        }
+      default:
+        println("WARN: Received unknown command \(header.command.toRaw()). Ignoring")
+    }
+  }
+
   // MARK: - Private Methods
 
   // Dequeues a message from the messageSendQueue and tries to send it. This should be called
@@ -252,111 +298,12 @@ public class PeerConnection: NSObject, NSStreamDelegate {
     }
     let bytesRead = inputStream.read(&readBuffer, maxLength:readBuffer.count)
     if bytesRead > 0 {
-      receivedBytes += readBuffer[0..<bytesRead]
-      processReceivedBytes()
+      messageParser.parseBytes([UInt8](readBuffer[0..<bytesRead]))
       if inputStream.hasBytesAvailable {
         networkThread.addOperationWithBlock {
           self.receive()
         }
       }
-    }
-  }
-
-  // Helper method for receive().
-  // Consumes the data in receivedBytes by parsing it into Messages, or discarding invalid data.
-  // Doesn't return until all data in receivedBytes has been consumed, or until more data must be
-  // received to parse a valid message.
-  private func processReceivedBytes() {
-    while true {
-      if receivedHeader == nil {
-        let headerStartIndex = headerStartIndexInBytes(receivedBytes)
-        if headerStartIndex < 0 {
-          // We did not find the message start, so we must wait for more bytes.
-          // Throw away the bytes we have since we couldn't figure out how to handle them.
-          // We might have all but the last byte of the networkMagicBytes for the next message,
-          // so keep the last 3 bytes.
-          let end = receivedBytes.count - network.magicBytes.count + 1
-          if end > 0 {
-            receivedBytes.removeRange(0..<end)
-          }
-          return
-        }
-        // Remove the bytes before startIndex since we don't know what they are.
-        receivedBytes.removeRange(0..<headerStartIndex)
-        if receivedBytes.count < Message.Header.length {
-          // We're expecting a header, but there aren't enough bytes yet to parse one.
-          // Wait for more bytes to be received.
-          return
-        }
-        let data = NSData(bytes:receivedBytes, length:receivedBytes.count)
-        receivedHeader = Message.Header.fromData(data)
-        if receivedHeader == nil {
-          // Failed to parse the header for some reason. It's possible that the networkMagicBytes
-          // coincidentally appeared in the byte data, or the header was invalid for some reason.
-          // Strip the networkMagicBytes so we can advance and try to parse again.
-          receivedBytes.removeRange(0..<network.magicBytes.count)
-          continue
-        }
-        // receivedHeader is guaranteed to be non-nil at this point.
-        // We successfully parsed the header from receivedBytes, so remove those bytes.
-        receivedBytes.removeRange(0..<Message.Header.length)
-      }
-      assert(Message.Header.length == 24)
-      // NOTE: payloadLength can be 0 for some message types, e.g. VersionAck.
-      let payloadLength = Int(receivedHeader!.payloadLength)
-      // TODO: Need to figure out a maximum length to allow here, or somebody could DOS us by
-      // providing a huge value for payloadLength.
-      if receivedBytes.count < payloadLength {
-        // Haven't received the whole message yet. Wait for more bytes.
-        return
-      }
-      let payloadData = NSData(bytes:receivedBytes, length:payloadLength)
-      processMessageWithHeader(receivedHeader!, payloadData:payloadData)
-      receivedBytes.removeRange(0..<payloadLength)
-      receivedHeader = nil
-    }
-  }
-
-  // Parses the payload from payloadData given the provided header, and notifies the delegate if
-  // parsing was successful. For some message types (e.g. VersionAck), payloadData is expected to
-  // have a length of 0.
-  private func processMessageWithHeader(header: Message.Header, payloadData: NSData) {
-    // TODO: Add the rest of the messages.
-    println("Received \(header.command.toRaw()) message")
-    switch header.command {
-      case .Version:
-        if peerVersion != nil {
-          println("WARN: Received extraneous VersionMessage. Ignoring")
-          break
-        }
-        assert(status == .Connecting)
-        let versionMessage = VersionMessage.fromData(payloadData)
-        if versionMessage == nil {
-          disconnectWithError(errorWithCode(.Unknown))
-          break
-        }
-        if !isPeerVersionSupported(versionMessage!) {
-          disconnectWithError(errorWithCode(.UnsupportedPeerVersion))
-          break
-        }
-        peerVersion = versionMessage!
-        sendVersionAck()
-        if receivedVersionAck {
-          didConnect()
-        }
-      case .VersionAck:
-        if status != .Connecting {
-          // The connection might have been cancelled, or it might have failed. For example,
-          // the connection can fail if we received an invalid VersionMessage from the peer.
-          println("WARN: Ignoring VersionAck message because not in Connecting state")
-          return
-        }
-        receivedVersionAck = true
-        if peerVersion != nil {
-          didConnect()
-        }
-      default:
-        println("WARN: Received unknown command \(header.command.toRaw()). Ignoring")
     }
   }
 
@@ -379,28 +326,6 @@ public class PeerConnection: NSObject, NSStreamDelegate {
       }
       NSThread.exit()
     }
-  }
-
-  // Returns -1 if the header start (the position of network.magicBytes) was not found.
-  // Otherwise returns the position where the message header begins.
-  private func headerStartIndexInBytes(bytes: [UInt8]) -> Int {
-    let networkMagicBytes = network.magicBytes
-    if bytes.count < networkMagicBytes.count {
-      return -1
-    }
-    for i in 0...(bytes.count - networkMagicBytes.count) {
-      var found = true
-      for j in 0..<networkMagicBytes.count {
-        if bytes[i + j] != networkMagicBytes[j] {
-          found = false
-          break
-        }
-      }
-      if found {
-        return i
-      }
-    }
-    return -1
   }
 
   private func didConnect() {
