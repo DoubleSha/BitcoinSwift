7eafe11b0a96997ed79a693f469b313e266cd624
Kevin Greene <kgreenek@gmail.com>
2014-12-03 22:51:54 -0800

    Initial PeerController implementation that just sync headers. It's very rough; submitting for 12 apps of swiftmas demo.

--- BitcoinSwift/PeerController.swift
+++ BitcoinSwift/PeerController.swift
@@ -8,29 +8,71 @@
 
 import Foundation
 
+public protocol PeerControllerDelegate: class {
+  func blockChainSyncComplete()
+}
+
 public class PeerController {
 
-  private let version: VersionMessage
+  private let hostname: String
+  private let port: UInt16
+  private let network: Message.Network
   private let queue: NSOperationQueue
   private let blockStore: SPVBlockStore
-  private let peerConnection: PeerConnection
+  private var connection: PeerConnection?
+  private weak var delegate: PeerControllerDelegate?
 
   public init(hostname: String,
               port: UInt16,
               network: Message.Network,
-              version: VersionMessage,
               blockStore: SPVBlockStore,
-              queue: NSOperationQueue = NSOperationQueue.mainQueue()) {
-    self.version = version
-    self.queue = queue
+              queue: NSOperationQueue = NSOperationQueue.mainQueue(),
+              delegate: PeerControllerDelegate? = nil) {
+    self.hostname = hostname
+    self.port = port
+    self.network = network
     self.blockStore = blockStore
-    self.peerConnection = PeerConnection(hostname: hostname,
-                                         port: port,
-                                         network: network)
+    self.queue = queue
+    self.delegate = delegate
   }
 
-  public func downloadHeaders() {
+  public func start() {
+    precondition(connection == nil)
+    queue.addOperationWithBlock {
+      self.connection = PeerConnection(hostname: self.hostname,
+                                       port: self.port,
+                                       network: self.network,
+                                       delegate: self,
+                                       delegateQueue: self.queue)
+      self.connection!.connectWithVersionMessage(self.createVersion())
+    }
+  }
 
+  private func createVersion() -> VersionMessage {
+    let senderPeerAddress = PeerAddress(services: PeerServices.NodeNetwork,
+                                        IP: IPAddress.IPV4(0x00000000),
+                                        port: 0)
+    let receiverPeerAddress = PeerAddress(services: PeerServices.NodeNetwork,
+                                          IP: IPAddress.IPV4(0x00000000),
+                                          port: 0)
+    return VersionMessage(protocolVersion: 70002,
+                          services: PeerServices.NodeNetwork,
+                          date: NSDate(),
+                          senderAddress: senderPeerAddress,
+                          receiverAddress: receiverPeerAddress,
+                          nonce: 0x5e9e17ca3e515405,
+                          userAgent: "/BitcoinSwift:0.0.1/",
+                          blockStartHeight: 0,
+                          announceRelayedTransactions: false)
+  }
+
+  private var genesisBlockHash: NSData {
+    let bytes: [UInt8] = [
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x19, 0xd6, 0x68,
+        0x9c, 0x08, 0x5a, 0xe1, 0x65, 0x83, 0x1e, 0x93,
+        0x4f, 0xf7, 0x63, 0xae, 0x46, 0xa2, 0xa6, 0xc1,
+        0x72, 0xb3, 0xf1, 0xb6, 0x0a, 0x8c, 0xe2, 0x6f]
+    return NSData(bytes: bytes, length: bytes.count).reversedData
   }
 }
 
@@ -38,16 +80,41 @@ extension PeerController: PeerConnectionDelegate {
 
   public func peerConnection(peerConnection: PeerConnection,
                              didConnectWithPeerVersion peerVersion: VersionMessage) {
-
+    queue.addOperationWithBlock {
+      let getHeadersMessage = GetHeadersMessage(protocolVersion: 70002,
+                                                blockLocatorHashes: [self.genesisBlockHash])
+      self.connection?.sendMessageWithPayload(getHeadersMessage)
+    }
   }
 
   public func peerConnection(peerConnection: PeerConnection,
                              didDisconnectWithError error: NSError?) {
-
+    Logger.info("Connection failed")
   }
 
   public func peerConnection(peerConnection: PeerConnection,
                              didReceiveMessage message: PeerConnectionMessage) {
-
+    switch message {
+      case .InventoryMessage(let inventoryMessage):
+        Logger.info("Inventory Message")
+        for inventoryVector in inventoryMessage.inventoryVectors {
+          Logger.info("  " + inventoryVector.description)
+        }
+      case .HeadersMessage(let headersMessage):
+        Logger.info("Received \(headersMessage.headers.count) block headers")
+        // If there are still more headers to sync, request more.
+        if headersMessage.headers.count == 2000 {
+          queue.addOperationWithBlock {
+            let lastHeaderHash = headersMessage.headers.last!.hash.reversedData
+            let getHeadersMessage = GetHeadersMessage(protocolVersion: 70002,
+                                                      blockLocatorHashes: [lastHeaderHash])
+            self.connection?.sendMessageWithPayload(getHeadersMessage)
+          }
+        } else {
+          Logger.info("Header sync complete!")
+        }
+      default:
+        break
+    }
   }
 }
