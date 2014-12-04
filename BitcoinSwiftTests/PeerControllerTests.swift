ca819777ae34dd627cfccf0fb13644308a790753
Kevin Greene <kgreenek@gmail.com>
2014-12-03 22:50:48 -0800

    Adding PeerControllerTests. Enabling install for BitcoinSwiftOSX framework target.

---
+++ BitcoinSwiftTests/PeerControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  PeerControllerTests.swift
+//  BitcoinSwift
+//
+//  Created by Kevin Greene on 12/2/14.
+//  Copyright (c) 2014 DoubleSha. All rights reserved.
+//
+
+import BitcoinSwift
+import XCTest
+
+class PeerControllerTests: XCTestCase {
+
+  // TODO
+}
