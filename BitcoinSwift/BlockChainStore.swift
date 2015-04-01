79b455c969fffdd3ec9540e6e7b6428888997e9c
Kevin Greene <kgreenek@gmail.com>
2015-03-31 21:06:23 -0700

    Comments

--- BitcoinSwift/BlockChainStore.swift
+++ BitcoinSwift/BlockChainStore.swift
@@ -20,25 +20,25 @@ public protocol BlockChainStoreParameters {
 public protocol BlockChainStore {
 
   /// The height of the current longest blockchain. If there is no head block, returns nil. If an
-  /// error occurs, it will be stored in error.
+  /// error occurs, it will be stored in |error|.
   func height(error: NSErrorPointer) -> UInt32?
 
   /// The head block in the current longest blockchain. If there is no head block, returns nil.
-  /// If an error occurs, it will be stored in error.
+  /// If an error occurs, it will be stored in |error|.
   func head(error: NSErrorPointer) -> BlockChainHeader?
 
   /// Adds blockChainHeader to the store and sets it as the head of the current longest chain.
-  /// If an error occurs, it will be stored in error.
+  /// If an error occurs, it will be stored in |error|.
   func addBlockChainHeaderAsNewHead(blockChainHeader: BlockChainHeader, error: NSErrorPointer)
 
   // TODO: Make this operate like a stack - so you can only delete the head blockHeader. Otherise
   // this can easily get into a bad state where the head is not updated after deleting a block.
   //
   /// Deletes the blockChainheader with |hash| from the store.
-  /// If not present, then this is just a NOP. If an error occurs, it will be stored in error.
+  /// If not present, then this is just a NOP. If an error occurs, it will be stored in |error|.
   func deleteBlockChainHeaderWithHash(hash: SHA256Hash, error: NSErrorPointer)
 
   /// Returns the blockChainHeader with the given hash. If there is no block matching the given
-  /// hash, returns nil. If an error occurs, it will be stored in error.
+  /// hash, returns nil. If an error occurs, it will be stored in |error|.
   func blockChainHeaderWithHash(hash: SHA256Hash, error: NSErrorPointer) -> BlockChainHeader?
 }
