25c0fce623a8ffbd67e8f9f27118fe0ac2bda12b
Kevin Greene <kgreenek@gmail.com>
2015-05-24 00:05:10 -0700

    Exposing Logger.outputFileHandle as a public method, so a custom file location can be specified for log messages. Also added comments in Logger.

--- BitcoinSwift/Logger.swift
+++ BitcoinSwift/Logger.swift
@@ -9,11 +9,13 @@
 import Foundation
 
 /// Use this class to write log message. By default log messages are written to stderr.
-/// TODO: Add support for logging to a file.
+/// To log to a custom file location, set outputFileHandle.
 public class Logger {
 
   private static let queue = NSOperationQueue()
-  private static var outputFileHandle = NSFileHandle.fileHandleWithStandardError()
+
+  /// The location where log messages will be written.
+  public static var outputFileHandle = NSFileHandle.fileHandleWithStandardError()
 
   public enum LogLevel: String {
     case Debug     = "DEBUG   "
@@ -26,38 +28,57 @@ public class Logger {
     case Emergency = "EMERGENCY"
   }
 
+  /// Log a message with log level .Debug.
+  /// The log message will be written to the location specified by Logger.outputFileHandle.
   public class func debug(message: String) {
     Logger.log(.Debug, message: message)
   }
 
+  /// Log a message with log level .Info.
+  /// The log message will be written to the location specified by Logger.outputFileHandle.
   public class func info(message: String) {
     Logger.log(.Info, message: message)
   }
 
+  /// Log a message with log level .Notice.
+  /// The log message will be written to the location specified by Logger.outputFileHandle.
   public class func notice(message: String) {
     Logger.log(.Notice, message: message)
   }
 
+  /// Log a message with log level .Warn.
+  /// The log message will be written to the location specified by Logger.outputFileHandle.
   public class func warn(message: String) {
     Logger.log(.Warn, message: message)
   }
 
+  /// Log a message with log level .Error.
+  /// The log message will be written to the location specified by Logger.outputFileHandle.
   public class func error(message: String) {
     Logger.log(.Error, message: message)
   }
 
+  /// Log a message with log level .Critical.
+  /// The log message will be written to the location specified by Logger.outputFileHandle.
   public class func critical(message: String) {
     Logger.log(.Critical, message: message)
   }
 
+  /// Log a message with log level .Alert.
+  /// The log message will be written to the location specified by Logger.outputFileHandle.
   public class func alert(message: String) {
     Logger.log(.Alert, message: message)
   }
 
+  /// Log a message with log level .Emergency.
+  /// The log message will be written to the location specified by Logger.outputFileHandle.
   public class func emergency(message: String) {
     Logger.log(.Emergency, message: message)
   }
 
+  /// Log a message the the specified log level. In general, you should use one of the other
+  /// methods for logging rather than calling this directly (e.g. Logger.info()).
+  /// The log message will be written to the location specified by Logger.outputFileHandle.
   public class func log(logLevel: LogLevel, message: String) {
     Logger.queue.addOperationWithBlock {
       Logger.outputFileHandle.writeData(
